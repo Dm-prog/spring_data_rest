efa28b25c723229a7e68f05ee3f29886da925116
Dm-prog <prostokota@list.ru>
2021-03-30 19:20:20 +0300

    Adding the entire project

---
+++ src/main/java/com/dmitriy/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.dmitriy.spring.springboot.spring_data_rest.dao;
+
+import com.dmitriy.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
